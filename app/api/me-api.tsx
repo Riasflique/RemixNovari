11d42f280da72a0a36781a6aae0267fb40598630
kraft <jesprkraft@gmail.com>
2024-04-10 16:10:07 +0200

    Branch + testkode

--- app/api/me-api.tsx
+++ app/api/me-api.tsx
@@ -6,11 +6,13 @@ class MeApi {
     static getUserInfo(): any {
       throw new Error("Method not implemented.");
     }
+    
     static async fetchDisplayName() {
-         const response = await fetch(`http://10.38.5.19:8080/resource`);
+         const response = await fetch(`http://localhost:8080/resource`);
              if (response.ok) {
-                console.log(response);
+                //console.log(response);
                  const data = await response.json();
+                 console.log(data)
                 return data;
              } else {
                  // Handle error response
@@ -20,10 +22,21 @@ class MeApi {
 
     }
     
-
-    static test() {
-
-        return "Hello world";
-    }
+/* EKSEMPEL
+    const sendStringToJava = async (str) => {
+        const response = await fetch('http://localhost:8080/testString', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ string: str })
+        });
+        const data = await response.json();
+        console.log(data); // Assuming Java backend sends a response in JSON format
+      };
+      
+      // Example usage:
+      sendStringToJava('Hello from Remix!');
+      */
 }
 export default MeApi;
