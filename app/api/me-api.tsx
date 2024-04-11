d109c4b3015ea948515efc6716da0b5306f001a1
kraft <jesprkraft@gmail.com>
2024-04-11 13:03:17 +0200

    Nå funker det ikke igjen, men er på vei

--- app/api/me-api.tsx
+++ app/api/me-api.tsx
@@ -1,3 +1,5 @@
+import {useEffect } from "react";
+
 const API_URL = process.env.API_URL;
 // Gutta - bruker
 // Gutta123 - passord
@@ -24,21 +26,3 @@ class MeApi {
 
 }
 export default MeApi;
-
-// Dette dukker opp i konsollen i IntelliJ en gang når npm run dev kjører
-const detteSendes = { ressurs: "Fakturamottaker" } // denne hentes fra buttonclick??
-// detteSendes kan skrives direkte i stringify
-const toJava = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ detteSendes })
-};
-fetch('http://localhost:8080/api/resourceString', toJava)
-  .then(response => {
-    if (!response.ok) {
-        throw new Error('Response ikke OK');
-    }
-  })
-  .catch(error => {
-    console.error('Fetch error:', error);
-  });
