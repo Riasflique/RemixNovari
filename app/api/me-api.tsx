5cd9d35afd85ad64c92d1083fde691060469b44c
FIRST_NAME LAST_NAME <eliaskkristensen@gmail.com>
2024-04-17 09:13:06 +0200

    nada

--- app/api/me-api.tsx
+++ app/api/me-api.tsx
@@ -1,25 +1,29 @@
+import { method } from "node_modules/cypress/types/bluebird";
+
 const API_URL = process.env.API_URL;
 // Gutta - bruker
 // Gutta123 - passord
+
+const baseURL = "http://localhost:8080/ansatte";
 
 class MeApi {
     static getUserInfo(): any {
       throw new Error("Method not implemented.");
     }
     static async fetchDisplayName() {
-         const response = await fetch(`http://10.38.5.19:8080/resource`);
+         const response = await fetch(`http://localhost:5432/resourceString/resource`);
              if (response.ok) {
-                console.log(response);
-                 const data = await response.json();
+                const data = await response.json();
+                //Check if response is ok
+                console.log(response); 
                 return data;
              } else {
                  // Handle error response
                  console.error("Error fetching display name");
                  throw new Error("Error fetching display name");
            }
-
+        
     }
-    
 
     static test() {
 
